03a5a164a742c3e514e6912af5c9a261a961aed6
Joseph Jabbour <jabbour.joseph@gmail.com>
2021-10-03 14:37:47 +0300

    added automapper profile

---
+++ CommandService/Profiles/CommandsProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using CommandService.Models;
+using CommandService.Dtos;
+
+namespace CommandService.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();            
+        }
+    }
+}
