7b64785fba8ef25dff300644515bd149db05d2be
Joseph Jabbour <jabbour.joseph@gmail.com>
2021-10-10 12:29:38 +0300

    added grpc client to the command service

--- CommandService/Profiles/CommandsProfile.cs
+++ CommandService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandService.Models;
 using CommandService.Dtos;
+using PlatformService;
 
 namespace CommandService.Profiles
 {
@@ -14,6 +15,10 @@ namespace CommandService.Profiles
             CreateMap<CommandCreateDto, Command>();    
             CreateMap<PlatformPublishedDto, Platform>()
                 .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.PlatfromId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands, opt => opt.Ignore());                
         }
     }
 }
